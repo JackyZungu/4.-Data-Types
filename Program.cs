1e40daf97165a62b199afef9576585b8fe5831c3
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-09 11:28:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jacky";
+            int age = 30;
+            bool isTall = false;
+            double weight = 80.2;
+
+            Console.WriteLine(name + " is " + age + " years he weight " + weight);
+            Console.WriteLine(" it is " + isTall + " that he is all.");
+
+            Console.ReadLine();
+        }
+    }
+}
